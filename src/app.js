b93a1f690d03834f8d20b4661847642fb510c646
SergioGala <sergiogalafdz@gmail.com>
2024-05-29 18:48:48 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,37 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = [
+    "Vincent Van Gogh ",
+    "Pablo Picasso ",
+    "Claude Monet",
+    "Francisco de Goya"
+  ];
+  let action = ["destroyed", "burned", "crack", "exterminated"];
+  let what = ["my carrer", "my fayth", "my hope", "my empire"];
+  let when = [
+    " in my wedding",
+    " in my prime",
+    "in my decadence",
+    " in my free day"
+  ];
+
+  const generateExcuse = () => {
+    let randomdecimal = Math.random();
+    let indicewho = Math.floor(randomdecimal * who.length);
+    let indiceaction = Math.floor(randomdecimal * action.length);
+    let indicewhat = Math.floor(randomdecimal * what.length);
+    let indicewhen = Math.floor(randomdecimal * when.length);
+
+    let wordwho = who[indicewho];
+    let wordaction = action[indiceaction];
+    let wordwhat = what[indicewhat];
+    let wordwhen = when[indicewhen];
+
+    let sentence = wordwho + " " + wordaction + " " + wordwhat + " " + wordwhen;
+
+    return sentence;
+  };
+
+  document.querySelector("#excuse").innerHTML = generateExcuse();
 };
